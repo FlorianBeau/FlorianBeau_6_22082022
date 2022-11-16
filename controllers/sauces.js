6df29f080378950a3fa4fa63be656f9d5a13a366
Florian Beau <florian.beau6942@gmail.com>
2022-11-16 16:17:46 +0100

    Likes and Dislikes finished

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -1,5 +1,7 @@
 const Sauce = require("../models/sauce");
 const fs = require("fs");
+const { findOneAndDelete } = require("../models/sauce");
+const { default: cluster } = require("cluster");
 
 exports.getAllSauces = (req, res, next) => {
   Sauce.find()
@@ -107,25 +109,86 @@ exports.deleteById = (req, res, next) => {
 };
 
 exports.postLike = (req, res, next) => {
-  //////////////////////// RESTE A FAIRE ///////////////////
-  // Il faudra l'userId
-  // Requête body: { userId: String, like: Number }
-  // Type de réponse attendue:
-  // const userLiked = JSON.parse(req.body.sauce);
-  /*
-  const like = {
-    userId: req.token.userId,
-  };
+  // 1 récupérer la sauce par son ID
+  // 2 si utilisateur à aimé la sauce, on ajoute 1 dans likes et son userId dans UserLike
+  // 3 si utilisateur n'a pas aimé la sauce, on ajoute 1 dans dislikes et son UserId dans UserDislikes
+  // 4 si il retire sa réaction
+  // 4.1 = si userId est dans tableau UserLikes on retire le like
+  // 4.2 = sinon on retire le dislike
 
-  like
-    .save()
-    .then(() => {
-      */
-  res.status(200).json({ message: "" });
-  /*
-    })
-    .catch((error) => {
-      res.status(404).json({ error });
-    });
-      */
+  Sauce.findOne({
+    _id: req.params.id,
+  }).then((sauce) => {
+    //////////////////////////////
+
+    if (req.body.like == 1) {
+      // On vérifie si l'usersId est dans le tableau usersLiked
+      // si oui = message erreur
+      // si non = l'exécution du code continue
+      if (sauce.usersLiked.includes(req.token.userId)) {
+        return res.status(400).json({ message: "Sauce déjà likée" });
+      }
+      // On vérifie si l'usersId n'est pas déjà dans le tableau usersLiked, si il n'y est pas, on le
+      // rajoute dans le tableau usersLiked et on incrémente le compteur like
+      else if (sauce.usersDisliked.includes(req.token.userId)) {
+        return res.status(400).json({ message: "Sauce déjà dislikée" });
+      } else {
+        sauce.likes++;
+        sauce.usersLiked.push(req.token.userId);
+      }
+
+      /////////////////////////////////////////////////////////////////////
+    } else if (req.body.like == 0) {
+      // On vérifie si l'utilisateur n'avait ni liké, ni disLiké sa sauce (si son userId est présent
+      // dans usersLiked ou usersDisliked). Si oui, on décrémente le compteur et on retire son userId
+      // de usersLiked ou usersDisliked.
+      if (
+        !sauce.usersDisliked.includes(req.token.userId) &&
+        !sauce.usersLiked.includes(req.token.userId)
+      ) {
+        return res.status(400).json({ message: "Requête non authorisée" });
+      } else if (sauce.usersDisliked.includes(req.token.userId)) {
+        sauce.dislikes--;
+        sauce.usersDisliked.splice(
+          sauce.usersDisliked.indexOf(req.token.userId),
+          1
+        );
+      } else {
+        // Si l'userId est dans les likes
+        sauce.likes--;
+        sauce.usersLiked.splice(sauce.usersLiked.indexOf(req.token.userId), 1); ////////////////// ????????
+      }
+    } else if (req.body.like == -1) {
+      // On vérifie si l'usersId est dans le tableau usersLiked
+      // si oui = message erreur
+      // si non = l'exécution du code continue
+      if (sauce.usersLiked.includes(req.token.userId)) {
+        return res.status(400).json({ message: "Sauce déjà likée" });
+      } else if (sauce.usersDisliked.includes(req.token.userId)) {
+        return res.status(400).json({ message: "Sauce déjà dislikée" });
+        // On vérifie si l'usersId n'est pas déjà dans le tableau usersDisliked, si il n'y est pas, on le
+        // rajoute dans le tableau usersLiked et on incrémente le compteur like
+      } else {
+        sauce.dislikes++;
+        sauce.usersDisliked.push(req.token.userId);
+      }
+    }
+
+    sauce
+      .save()
+      .then((sauce) => {
+        res.status(200).json(sauce);
+        console.log(res.sauce);
+      })
+      .catch((error) => {
+        res.status(400).json({
+          error: error,
+        });
+      })
+      .catch((error) => {
+        res.status(404).json({
+          error: error,
+        });
+      });
+  });
 };
